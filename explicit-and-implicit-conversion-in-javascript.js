a07f949b97054f9980238dbbb823eec56042388e
Sarah <skat1403@gmail.com>
2025-03-30 23:23:02 -0500

    debugged and converted stuff

--- explicit-and-implicit-conversion-in-javascript.js
+++ explicit-and-implicit-conversion-in-javascript.js
@@ -18,15 +18,46 @@ Use console.log() to clearly show the before-and-after type conversions.
 
 */
 
+/*
+The result is the same, but I explicitly converted the string "5" to a number before subtracting 2
+*/
 
-let result = "5" - 2;
+let result = Number("5") - 2;
 console.log("The result is: " + result);
 
-let isValid = Boolean("false");
+/*
+let isValid = Boolean("false"); is evaluating false as a string, which is truthy.
+I removed the quotation marks to make it an actual Boolean value instead of a string.
+I could also change "false" to "true" which would make isValid truthy and print an expected result.
+*/
+let isValid = Boolean(false);
 if (isValid) {
     console.log("This is valid!");
 }
 
+/*
+age was initialized as a string so totalAge did a string concatination instead of number addition.
+I converted age to a number in the calculation so it would return the expected result of 30.
+*/
+
 let age = "25";
-let totalAge = age + 5;
+let totalAge = Number(age) + 5;
+
 console.log("Total Age: " + totalAge);
+
+
+let asANum = 9;
+console.log(asANum + " is a " + typeof(asANum));
+let asAString = String(asANum);
+console.log("Now " + asAString + " is a " + typeof(asAString));
+console.log("Now it is a " + Boolean(asANum) + ", or at least 'truthy,' " + typeof(Boolean(asANum)));
+
+
+let yourAge = '20';
+let myAge = 20;
+console.log("Are we the same age?");
+console.log(yourAge==myAge);
+
+console.log("Are our ages strictly equal?");
+console.log(yourAge===myAge);
+console.log("Your age is " + yourAge + ", which is a " + typeof(yourAge) + ", and mine is " + myAge + ", which is a " + typeof(myAge));
